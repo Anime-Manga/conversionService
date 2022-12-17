2a406cac7fa41c3531304eb2d9f2013666052a0e
Cesxhin <mattia.ceschin@gmail.com>
2022-12-17 17:52:45 +0100

    Add project

---
+++ README.md
@@ -0,0 +1,27 @@
+## 💱Conversion Service
+Questo progetto verrà utilizzato per convertire file ts in mp4 da poter riprodurre in streaming
+### Information general:
+- `require` volume mounted on Docker
+### Variabili globali richiesti:
+```sh
+example:
+    #--- rabbit ---
+    USERNAME_RABBIT: "guest" #guest [default]
+    PASSWORD_RABBIT: "guest" #guest [default]
+    ADDRESS_RABBIT: "localhost" #localhost [default]
+    LIMIT_CONSUMER_RABBIT: "5" #3 [default]
+    
+    #--- API ---
+    ADDRESS_API: "localhost" #localhost [default]
+    PORT_API: "33333" #3000 [default]
+    PROTOCOL_API: "http" or "https" #http [default]
+    
+    #--- Logger ---
+    LOG_LEVEL: "Debug|Info|Error" #Info [default]
+    WEBHOOK_DISCORD_DEBUG: "url" [not require]
+    
+    #--- General ---
+    PATH_TEMP: "/folder/temp" [require]
+    PATH_FFMPEG: "/folder/bin" #/usr/local/bin/ffmpeg [default]
+    BASE_PATH: "/folder/anime" or "D:\\\\Directory\Anime" #/ [default]
+```
---
+++ src/Cesxhin.AnimeManga.ConversionService/Program.cs
@@ -0,0 +1,73 @@
+using Cesxhin.AnimeManga.Application.Consumers;
+using Cesxhin.AnimeManga.Application.CronJob;
+using Cesxhin.AnimeManga.Application.Generic;
+using FFMpegCore;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NLog;
+using Quartz;
+using System;
+
+namespace Cesxhin.AnimeManga.ConversionService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    //rabbit
+                    services.AddMassTransit(
+                    x =>
+                    {
+                        x.UsingRabbitMq((context, cfg) =>
+                        {
+                            cfg.Host(
+                                Environment.GetEnvironmentVariable("ADDRESS_RABBIT") ?? "localhost",
+                                "/",
+                                credentials =>
+                                {
+                                    credentials.Username(Environment.GetEnvironmentVariable("USERNAME_RABBIT") ?? "guest");
+                                    credentials.Password(Environment.GetEnvironmentVariable("PASSWORD_RABBIT") ?? "guest");
+                                });
+
+                            cfg.ReceiveEndpoint("conversion", e => {
+                                e.Consumer<ConversionConsumer>(cc =>
+                                {
+                                    string limit = Environment.GetEnvironmentVariable("LIMIT_CONSUMER_RABBIT") ?? "3";
+
+                                    cc.UseConcurrentMessageLimit(int.Parse(limit));
+                                });
+                            });
+
+                        });
+                    });
+
+                    //setup nlog
+                    var level = Environment.GetEnvironmentVariable("LOG_LEVEL").ToLower() ?? "info";
+                    LogLevel logLevel = NLogManager.GetLevel(level);
+                    NLogManager.Configure(logLevel);
+
+                    //cronjob for check health
+                    services.AddQuartz(q =>
+                    {
+                        q.UseMicrosoftDependencyInjectionJobFactory();
+                        q.ScheduleJob<HealthJob>(trigger => trigger
+                            .StartNow()
+                            .WithDailyTimeIntervalSchedule(x => x.WithIntervalInSeconds(60)), job => job.WithIdentity("conversion"));
+                    });
+                    services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+                    //set ffmpeg
+                    GlobalFFOptions.Configure(options => options.BinaryFolder = Environment.GetEnvironmentVariable("PATH_FFMPEG").ToLower() ?? "./bin");
+
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
